900971f1dd2b79223999bf6b9f162294a20d9e10
Luan Carlos <luan.k28@hotmail.com>
2025-12-05 21:35:10 -0300

    Commit desafios OrdenacaoPessoas e OrdenacaoNumeros

---
+++ src/br/com/upgrade/list/ordenacao/ordenacaopessoas/OrdenacaoPessoaTeste.java
@@ -0,0 +1,19 @@
+package br.com.upgrade.list.ordenacao.ordenacaopessoas;
+
+public class OrdenacaoPessoaTeste {
+
+    public static void main(String[] args) {
+        OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+
+        ordenacaoPessoas.adicionarPessoa("Alice", 20, 1.56);
+        ordenacaoPessoas.adicionarPessoa("Bob", 30, 1.80);
+        ordenacaoPessoas.adicionarPessoa("Charlie", 25, 1.70);
+        ordenacaoPessoas.adicionarPessoa("David", 17, 1.56);
+
+        System.out.println(ordenacaoPessoas.pessoas);
+
+        System.out.println(ordenacaoPessoas.ordenarPorIdade());
+
+        System.out.println(ordenacaoPessoas.ordenarPorAltura());
+    }
+}
